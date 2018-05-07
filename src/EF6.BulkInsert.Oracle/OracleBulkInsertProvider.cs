d96d8356fdb90a2357e07ecd84536f4468462943
Victor Ortuondo <vitor.m.o@gmail.com>
2018-05-07 19:29:23 +0200

    Added Oracle support.

---
+++ src/EF6.BulkInsert.Oracle/OracleBulkInsertProvider.cs
@@ -0,0 +1,38 @@
+﻿using EF6.BulkInsert.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6.BulkInsert.Oracle
+{
+    public class OracleBulkInsertProvider : DefaultBulkInsertProvider
+    {
+        public OracleBulkInsertProvider()
+        {
+            SetProviderIdentifier("Oracle.ManagedDataAccess.Client.OracleConnection");
+        }
+        protected override string CreateInsertBatchText(string insertHeader, List<string> rows)
+        {
+            return $"BEGIN \n{base.CreateInsertBatchText(insertHeader, rows)}\nEND;";
+        }
+
+        protected override void AddParameter(Type type, object value, List<string> values)
+        {
+            if (this.IsDateType(type))
+            {
+                if (value is DateTime)
+                {
+                    values.Add($"TO_DATE('{((DateTime)value).ToString("yyyy-MM-dd HH:mm:ss")}', 'YYYY-MM-DD HH24:MI:SS')");
+                }
+                else if (value is DateTimeOffset)
+                {
+                    values.Add($"TO_DATE('{((DateTimeOffset)value).ToString("yyyy-MM-dd HH:mm:ss")}', 'YYYY-MM-DD HH24:MI:SS')");
+                }
+            }
+            else
+                base.AddParameter(type, value, values);
+        }
+    }
+}
