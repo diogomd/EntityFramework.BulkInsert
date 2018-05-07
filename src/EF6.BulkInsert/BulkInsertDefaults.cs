d0a4d0963a88143d1be3798fed8d64b96c3a07bd
Victor Ortuondo <vitor.m.o@gmail.com>
2018-05-07 16:31:50 +0200

    Reduced the default batch size.

--- src/EF6.BulkInsert/BulkInsertDefaults.cs
+++ src/EF6.BulkInsert/BulkInsertDefaults.cs
@@ -2,7 +2,7 @@
 {
     public static class BulkInsertDefaults
     {
-        public static int BatchSize = 5000;
+        public static int BatchSize = 500;
         public static BulkCopyOptions BulkCopyOptions = BulkCopyOptions.Default;
         public static int TimeOut = 30;
         public static int NotifyAfter = 1000;
